be47d0cbe97520059bc2abf2fe25a720fe7b10f7
alepbloyd <alexanderleeboyd@gmail.com>
2022-04-19 17:46:57 -0400

    Add float variables

--- variables.rb
+++ variables.rb
@@ -9,3 +9,9 @@ location = "Washington, DC"
 days_in_year = 365
 feet_in_mile = 5280
 inches_in_mile = 63360
+
+#floats
+
+feet_in_meter = 3.281
+feet_in_kilometer = 3280.84
+centimeters_in_inch = 2.54
